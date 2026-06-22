5d311da9d4236319f321d82bf8a996a71fd1759a
Jyoti <ilu29jan@gmail.com>
2026-06-22 16:37:17 +0530

    file updated

--- modules/eks/nodeGroup.tf
+++ modules/eks/nodeGroup.tf
@@ -6,31 +6,10 @@ resource "aws_eks_node_group" "main" {
   instance_types  = var.node_group_instance_types
   capacity_type   = "ON_DEMAND"
 
-data "aws_launch_template" "existing" {
-  name = var.launch_template_name
-}
-
-resource "aws_launch_template_version" "with_bootstrap" {
-  launch_template_id   = data.aws_launch_template.existing.id
-  version_description  = "add eks bootstrap user_data"
-  # Do NOT add iam_instance_profile or instance_type here for managed node groups
-  user_data = base64encode(<<-EOT
-    #!/bin/bash
-    set -x
-    exec > >(tee /var/log/bootstrap.log | logger -t userdata -s 2>/dev/console) 2>&1
-    if [ -f /etc/eks/bootstrap.sh ]; then
-      /etc/eks/bootstrap.sh ${aws_eks_cluster.main.name} --kubelet-extra-args '--v=4'
-    else
-      echo "/etc/eks/bootstrap.sh not found on AMI" >&2
-      exit 1
-    fi
-  EOT)
-}
-
-#   launch_template {
-#     id      = data.aws_launch_template.existing.id
-#     version = "$Latest"
-#   }
+  launch_template {
+    id      = data.aws_launch_template.existing.id
+    version = tostring(aws_launch_template_version.with_bootstrap.version_number)
+  }
 
   scaling_config {
     desired_size = var.node_group_desired_size
--- modules/eks/node_template.tf
+++ modules/eks/node_template.tf
@@ -1,39 +1,22 @@
 data "aws_launch_template" "existing" {
   name = var.launch_template_name
-#   user_data = base64encode("#!/bin/bash\nset -o xtrace\n/etc/eks/bootstrap.sh ${aws_eks_cluster.main.name}\n")
 }
 
-# resource "aws_iam_instance_profile" "node" {
-#   name = "${var.cluster_name}-${var.env}-node-profile"
-#   role = aws_iam_role.main.name
-# }
+resource "aws_launch_template_version" "with_bootstrap" {
+  launch_template_id  = data.aws_launch_template.existing.id
+  version_description = "add eks bootstrap user_data"
 
-# resource "aws_launch_template" "node" {
-#   name_prefix   = "${var.cluster_name}-${var.env}-lt-"
-#   image_id      = var.ami
-#   # do not hardcode instance_type if you want the node group to control instance types
-# #   iam_instance_profile {
-# #     name = aws_iam_instance_profile.node.name
-# #   }
-
-#   # simple bootstrap for EKS managed nodes - cluster name injected
-# #   user_data = base64encode(<<-EOT
-# #     #!/bin/bash
-# #     set -x
-# #     # send stdout/stderr to /var/log/bootstrap.log and to the console
-# #     exec > >(tee /var/log/bootstrap.log | logger -t userdata -s 2>/dev/console) 2>&1
-# #     date
-# #     echo "Checking for bootstrap script..."
-# #     if [ -f /etc/eks/bootstrap.sh ]; then
-# #       echo "Found bootstrap, running with increased kubelet verbosity"
-# #       /etc/eks/bootstrap.sh ${aws_eks_cluster.main.name} --kubelet-extra-args '--v=4'
-# #       rc=$?
-# #       echo "bootstrap.sh exit code: ${rc}"
-# #       exit ${rc}
-# #     else
-# #       echo "/etc/eks/bootstrap.sh not found on AMI" >&2
-# #       exit 1
-# #     fi
-# #   EOT
-# #   )
-# }
+  # Do NOT set iam_instance_profile or instance_type here for managed node groups
+  user_data = base64encode(<<-EOT
+    #!/bin/bash
+    set -x
+    exec > >(tee /var/log/bootstrap.log | logger -t userdata -s 2>/dev/console) 2>&1
+    if [ -f /etc/eks/bootstrap.sh ]; then
+      /etc/eks/bootstrap.sh ${aws_eks_cluster.main.name} --kubelet-extra-args '--v=4'
+    else
+      echo "/etc/eks/bootstrap.sh not found on AMI" >&2
+      exit 1
+    fi
+  EOT
+  )
+}
