6ab400bb68e9e7776bb708c043da9d495363e688
Jyoti <ilu29jan@gmail.com>
2026-06-22 12:41:24 +0530

    file updated

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ environment=$1
 action=$2
 rm -rf .terraform ;
 terraform init -backend-config=env/${environment}/state.tfvars ;
-terraform plan -auto-approve --var-file=env/${environment}/${environment}.tfvars ;
+terraform plan --var-file=env/${environment}/${environment}.tfvars ;
 terraform $action -auto-approve --var-file=env/${environment}/${environment}.tfvars 
