4ad05d33f9752b4aefb5cb9eb5211a6caac1fc89
Jyoti <ilu29jan@gmail.com>
2026-06-23 09:41:26 +0530

    file updated

--- init.sh
+++ init.sh
@@ -2,15 +2,15 @@
 
 
 environment=$1
-# action=$2
+action=$2
 rm -rf .terraform ;
 terraform init -backend-config=env/${environment}/state.tfvars ;
 terraform plan --var-file=env/${environment}/${environment}.tfvars ;
-# terraform $action -auto-approve --var-file=env/${environment}/${environment}.tfvars
+terraform $action -auto-approve --var-file=env/${environment}/${environment}.tfvars
 
-# if [ $? -ne 0 ]; then
-#    echo -e "\e[31m Terraform $action failed! \e[0m"
-#    exit 1
-# else
-#    echo -e "\e[32m Terraform $action completed successfully! \e[0m"
-# fi
+if [ $? -ne 0 ]; then
+   echo -e "\e[31m Terraform $action failed! \e[0m"
+   exit 1
+else
+   echo -e "\e[32m Terraform $action completed successfully! \e[0m"
+fi
